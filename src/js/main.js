e3ca3da96240e1a2f9b879f29b510a3708e76a19
Hlaing Hpone <leoknowledgesharing@gmail.com>
2024-01-15 14:08:53 +0630

    header upload

--- src/js/main.js
+++ src/js/main.js
@@ -8,3 +8,36 @@ import "@fortawesome/fontawesome-free/js/all.js";
 import "@fortawesome/fontawesome-free/css/all.css";
 
 
+const menu_btn = document.querySelector('.hamburger');
+	const mobile_menu = document.querySelector('.c-mobile-nav');
+
+	menu_btn.addEventListener('click', function () {
+		menu_btn.classList.toggle('is-active');
+		mobile_menu.classList.toggle('is-active');
+	});
+
+    
+import '../../node_modules/slick-carousel/slick/slick.css';
+
+import $ from 'jquery';
+import 'slick-carousel';
+
+$(document).ready(function(){
+    $('.slider').slick({
+        centerMode: true,
+        slidesToShow: 5,
+        infinte: true,
+        speed: 500,
+        prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
+        nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>',
+        responsive: [
+            {
+                breakpoint: 800,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+});
+
